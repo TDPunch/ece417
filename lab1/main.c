bf6b4d62ddb416405664e10627af782379ff960f
TDPunch <tylerdavidpunch@gmail.com>
2020-03-10 15:56:02 -0400

    Finished lab1

--- lab1/main.c
+++ lab1/main.c
@@ -1,8 +1,9 @@
 // This program does some basic Lab-Volt 5150 robot movementsw
 
-#include "/usr/linux/public/labvolt/labvolt.h"
+#include "/home/pi/ece417/labvolt/inc/labvolt.h"
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	init();
 
@@ -10,9 +11,17 @@ int main(int argc, char *argv[])
 
 	moverel(542, 0, 0, 0, 0);	// Rotate base 30 degrees CCW
 
-	moverel(0, -478, 478, 361, -361);
-	moverel(0, 0, 358, 271, -271);
-	moverel(0, 0, 0, );
+	moverel(0, -478, 478, 361, -361); // Rotate shoulder 20 degrees down
+	moverel(0, 0, 358, 271, -271);    // Rotate elbow 15 degrees in
+	moverel(0, 0, 0, -361, -361);	  // Roll wrist 20 degrees CW
+	moverel(0, 0, 0, -361, 361);	  // Pitch wrist 20 degrees up
 
-	shutdown();
+	sleep(5);						  // Sleep for 5 seconds
+
+	gripperClose();					  // Close the gripper
+	nest();							  // Return to home positions
+
+	shutdown();						  // Power down
+
+	return 0;
 }
