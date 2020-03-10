6b2cb5592ce9f9053af81d242b80eab5b09d2bd8
TDPunch <tylerpunchoh@gmail.com>
2020-03-10 12:09:04 -0400

    Finished lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,39 +1,87 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "mmult.h"
+// This is the main program that asks for 2 4x4 matrices and a point,
+// then calls functions that multiply the two matrices, transform the
+// point and take the inverse of a matrix respectively. All results
+// are output to stdout.
+
+#include <stdio.h>	/* Inlcude standard C library functions */
+#include <stdlib.h> /* malloc(), free()*/
+#include "mmult.h"  /* Include matrix manipulation functions */
 
 int main(int argc, char *argv[]) 
 {
+	/*
+	// Variable declaration and initialization
 	float **solution = NULL;
+	float *px = NULL;
 	float x = (2.0/7.0), y = (3.0/7.0), z = (6.0/7.0);
 
 	float a[4][4] = { 
-		{y, x, z, 4},
-		{x, z, -1*y, 5},
-	    {-1*z, y, x, 6},
-		{0, 0, 0, 1}	
+		{y,   	 x,   z, 	  4.0},
+		{x,		 z,   -1.0*y, 5.0},
+	    {-1.0*z, y,   x, 	  6.0},
+		{0.0, 	 0.0, 0.0, 	  1.0}	
 	};
 
 	float b[4][4] = {
-		{x, -1*z, y, 1},
-		{z, y, x, 2},
-		{-1*y, x, z, 3},
-		{0, 0, 0, 1}
+		{x, 	 -1.0*z, y,   1.0},
+		{z, 	 y, 	 x,   2.0},
+		{-1.0*y, x, 	 z,   3.0},
+		{0.0, 	 0.0,	 0.0, 1.0}
 	};
 
-	solution = matrix_mult(a, b);
+	float p[3] = {0.0,1.0,2.0};
 
+	// Call multiplication function and print result
+	solution = matrix_mult(a, b);
+	print_composite(solution);
+
+	// Call transform function and print result
+	px = point_trans(b, p);
+	print_point(px);
+
+	// Call inverse function and print result
+	solution = inverse(b);
+	print_composite(solution);
+	*/
+	
+	// Variable decleration and initialization
+	float a[4][4] = {0}, b[4][4] = {0}, p[3] = {0};
+	float **comp = NULL, *transform = NULL, **inv = NULL;
+	char xyz[3] = {'x','y','z'};
+
+	printf("Enter the first matrix:\n");
+
+	// Input matrix from user
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			printf("%f", solution[i][j]);
-			printf("\t");
+			printf("Enter matrix element [%d, %d]: ", i, j);
+			scanf("%f", &a[i][j]);
 		}
-		printf("\n");
 	}
 
+	printf("Enter the second matrix:\n");
+
+	// Input matrix from user
 	for (int i = 0; i < 4; i++) {
-		free(solution[i]);
+		for (int j = 0; j < 4; j++) {
+			printf("Enter matrix element [%d, %d]: ", i, j);
+			scanf("%f", &b[i][j]);
+		}
 	}
-	free(solution);
+
+	printf("Enter the point:\n");
+
+	// Input point from user
+	for (int i = 0; i < 3; i++) {
+		printf("Enter %c coordinate: ", xyz[i]);
+		scanf("%f", &p[i]);
+	}
+
+	print_matrix(matrix_mult(a, b));
+	printf("\n");
+	print_point(point_trans(b, p));
+	printf("\n");
+	print_matrix(inverse(b));
+
 	return 0;
 }
--- lab2/mmult.c
+++ lab2/mmult.c
@@ -4,26 +4,123 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
+// matrix_mult() computes the product of two matrices and allocates
+// memory for the solution
 float **matrix_mult(float A[4][4], float B[4][4])
 {
-	// Allocate memory for return array
+	// Allocate memory for the columns
 	float **C = malloc(sizeof(float *) * 4);
-	float sum = 0;
+	float sum = 0.0;
 
-	
+	// Allocate memory for rows
 	for (int i = 0; i < 4; i++) {
 		C[i] = malloc(sizeof(float *) * 4);
 	} 
 
+	// Multiply the matrices
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			for (int k = 0; k < 4; k++) {
 				sum = sum + A[i][k] * B[k][j];
 			}
 			C[i][j] = sum;
-			sum = 0;
+			sum = 0.0;
 		}
 	}
+
 	return C;
+}
+
+// point_trans() transforms a point using matrix R and allocates memory
+// for the transformed point
+float *point_trans(float R[4][4], float Puvw[3])
+{
+	// Allocate memory for return point in XYZ-space
+	float *Pxyz = malloc(sizeof(float *) * 3);
+	float sum = 0.0;
+
+	// Transform point to XYZ-space
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			sum = sum + R[i][j] * Puvw[j];
+		}
+		// Don't forget the last element
+		Pxyz[i] = sum + R[i][3];
+		sum = 0.0;
+	}
+	
+	return Pxyz;
+}
+
+// inverse() computes the inverse of a homogeneous transformation matrix
+float **inverse(float a[4][4])
+{
+	// Allocate memory for the columns
+	float **sol = malloc(sizeof(float *) * 4);
+	float sum = 0.0;
+
+	// Allocate memory for the rows
+	for (int i = 0; i < 4; i++) {
+		sol[i] = malloc(sizeof(float *) * 4);
+	}
+
+	// The inverse of the rotation portion is just the transpose
+	for (int row = 0; row < 3; row++) {
+		for (int col = 0; col < 3; col++) {
+			sol[row][col] = a[col][row];
+		}
+	}
+
+	// The inverse of the translation is -R(transpose) * T
+	// multpiply the transpose of the rotation portion by the 
+	// initial translation portion of the input matrix.
+	for (int row = 0; row < 3; row++) {
+		for (int col = 0; col < 3; col++) {
+			sum = sum + sol[row][col] * a[col][3];
+		}
+		sol[row][3] = -1.0 * sum;
+		sum = 0.0;
+	}
+
+	// 4th row is just (0, 0, 0, 1)
+	for (int col = 0 ; col < 4; col++) {
+		sol[3][col] = (col + 1) / 4;
+	}
+
+	return sol;	
+}
+
+// This function just prints a 4x4 matrix to verify above subroutines
+// are working properly, it also frees the allocated memory to keep
+// main shorter
+void print_matrix(float **m)
+{
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%f", m[i][j]);
+			printf("\t");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < 4; i++) {
+		free(m[i]);
+	}
+	free(m);
+}
+
+// This function just prints a 3 coordinate point to verify above subroutine
+// is working properly, it also frees the allocated memory to keep main
+// shorter
+void print_point(float *p)
+{
+	for (int i = 0; i < 3; i++) {
+		printf("%f", p[i]);
+		printf("\t");
+	}
+	printf("\n");
+
+	free(p);
 }
